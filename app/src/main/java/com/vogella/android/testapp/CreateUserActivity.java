19f63f3fe47bd1d7942277a04597a9772a85f8a6
Patrick Peters <pspeters93@gmail.com>
2019-03-06 16:45:29 -0600

    Transferring to laptop. Still working though exercise 10 in tutorials

---
+++ app/src/main/java/com/vogella/android/testapp/CreateUserActivity.java
@@ -0,0 +1,22 @@
+package com.vogella.android.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+
+    public void onClick(View view){
+        EditText input = (EditText) findViewById(R.id.username);
+        String string = input.getText().toString();
+        Toast.makeText(this, "User "+string+" created.", Toast.LENGTH_LONG).show();
+    }
+}
